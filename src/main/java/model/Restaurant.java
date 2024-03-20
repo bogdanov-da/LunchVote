0bad27f339bb457d6c5226d2e3cba3d1a6679430
bodm <bodm@gurtam.com>
2024-03-20 10:11:45 +0100

    add restaurant model

--- src/main/java/model/Restaurant.java
+++ src/main/java/model/Restaurant.java
@@ -4,4 +4,20 @@ import java.util.List;
 
 public class Restaurant extends AbstractNameEntity {
     private List<Menu> menus;
+
+    public Restaurant() {
+    }
+
+    public Restaurant(Integer id, String name, List<Menu> menus) {
+        super(id, name);
+        this.menus = menus;
+    }
+
+    public List<Menu> getMenus() {
+        return menus;
+    }
+
+    public void setMenus(List<Menu> menus) {
+        this.menus = menus;
+    }
 }
